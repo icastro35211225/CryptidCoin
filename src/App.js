fb844fa569d416168eec9749f2a7c329415535b8
Christian Salinas <cmsalinas2000@gmail.com>
2023-03-25 17:49:12 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar'
 import PageBody from './components/dummy';
-
+import Board from './components/leaderboard';
+import './components/board.css'
 function App() {
     return (
         <div className="App">
             <Navbar/>
             <PageBody/>
-
+            <Board/>
         </div>
     );
 }
