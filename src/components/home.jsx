920b0695a6978c08639d655a8861c3eae9c5742e
Jay Simmons <jaysimmons280@gmail.com>
2023-03-25 23:32:34 -0500

    Background Added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,15 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from '@react-three/drei';
-import Graphic from './Graphics';
+import THREE from '@react-three/fiber';
+//import Graphic from './Graphics';
+import Chest from './Chest';
+var mouse, raycaster;
+
 
 export default function Home() {
-
+mouse = new THREE.Vector2();
+raycaster = new THREE.Raycaster();
     return (
         <div className="canvas">
             <Canvas style={{ width: window.innerWidth, height: window.innerHeight }}>
@@ -13,10 +18,10 @@ export default function Home() {
                 <directionalLight position={[-2, 5, 2]} intensity={1} />
                 <Suspense fallback={null} />
                 <ambientLight intensity={0.5} />
-                <directionalLight position={[-2, 5, 2]} intensity={1} />
-                <Suspense fallback={null} />
-                <Graphic />
+                <Chest position = {[0,0,0]}/>
             </Canvas>
+            
         </div>
     )
 }
+
