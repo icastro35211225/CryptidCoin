61a5adbf97a5aa75d3c6d89c99fc43b9bf18cbd9
Ivan Castro <icastro3521@yahoo.com>
2023-03-25 21:17:30 -0500

    same as ivan-branch

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    FaTh,
+    FaBars,
+    FaUserAlt,
+    FaRegChartBar,
+    FaCommentAlt,
+    FaShoppingBag,
+    FaThList
+} from "react-icons/fa";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const menuItem = [
+        {
+            path: '/',
+            name: 'Home'
+        },
+        {
+            path: '/signup',
+            name: 'Sign Up'
+        },
+        {
+            path: '/login',
+            name: 'Log In'
+        }
+    ]
 
     return (
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-        </ul>
+        <>
+            <div className="container">
+                <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+                    <div className="top_section">
+                        <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+                            <FaBars onClick={toggle} />
+                        </div>
+                    </div>
+                    {
+                        menuItem.map((item, index) => (
+                            <Link to={item.path} key={index} className="link" activeclassName="active">
+                                <div style={{ display: isOpen ? "block" : "none" }} className="link_text">{item.name}</div>
+                            </Link>
+                        ))
+                    }
+                </div>
+            </div>
+
+            {/* <ul>
+                <li><Link to="/">Home </Link></li>
+                <li><Link to="/signup">Sign up </Link></li>
+                <li><Link to="/">Home </Link></li>
+            </ul> */}
+        </>
     )
 }
