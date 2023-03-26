de8a482a00705ac9864528e8cfcf82d58d651cd0
Ivan Castro <icastro3521@yahoo.com>
2023-03-25 21:14:50 -0500

    yuh

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import Navbar from './components/navbar'
-import PageBody from './components/dummy';
-import './components/board.css'
-import Leaderboard from './components/leaderBoard';
-function App() {
+import React from "react";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './components/home';
+import SignUp from './components/signup';
+import Login from './components/login';
+
+
+export default function App() {
     return (
+
         <div className="App">
-            <Navbar />
-            <PageBody />
-            <Leaderboard />
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path='/signup' element={<SignUp />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
-
-export default App;
---
+++ src/components/signup.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+
+export default function SignUp() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function signup() {
+        if (username && password) {
+            axios.post('http://localhost:3001/signup', {
+                username: username + "",
+                password: password
+            }).then((response) => {
+                console.log(response);
+                // navigate('/');
+            })
+        }
+    }
+
+    return (
+        <>
+            <h1>Sign Up</h1>
+            <form>
+                <input id="name" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username"></input><br />
+                <input id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input><br />
+                <button type="submit" onClick={(e) => {
+                    e.preventDefault();
+                    signup();
+                }} >Sigup</button>
+            </form>
+        </>
+    )
+}
