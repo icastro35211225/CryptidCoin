056d754d59d882482a4fbd028fb0834a7744dd56
Ivan Castro <icastro3521@yahoo.com>
2023-03-25 20:08:52 -0500

    make size of screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar'
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from '@react-three/drei';
 import Graphic from './components/Graphics';
@@ -9,11 +9,11 @@ import Graphic from './components/Graphics';
 export default function App() {
     return (
         <div className="canvas">
-            <Canvas >
+            <Canvas style={{ width: window.innerWidth, height: window.innerHeight }}>
                 <OrbitControls enableZoom={true} />
-                <ambientLight intensity={0.5}/>
-                <directionalLight position={[-2,5,2]} intensity={1}/>
-                <Suspense fallback={null}/>
+                <ambientLight intensity={0.5} />
+                <directionalLight position={[-2, 5, 2]} intensity={1} />
+                <Suspense fallback={null} />
                 <Graphic />
             </Canvas>
         </div>
